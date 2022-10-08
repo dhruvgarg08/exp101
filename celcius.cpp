fb66d0052950a753767fba6253786b1e09be405e
dhruvgarg08 <dhruv.smk10@gmail.com>
2022-10-08 16:59:26 +0530

    celcius

---
+++ celcius.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int c;
+    cin>>c;
+    cout<<"enter value"<<endl;
+    double f;
+    f=(c*5/9)+32;
+    cout<<"value is"<<f<<endl;
+
+}
